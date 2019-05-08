e23aeba032af85ae32d726ba86e98ec5132269db
YU-AN CHEN <yuanchen@cs.nctu.edu.tw>
2019-05-08 16:05:21 +0800

    Update sudoku.cpp

--- sudoku/sudoku.cpp
+++ sudoku/sudoku.cpp
@@ -4,33 +4,32 @@ using namespace std;
 
 inline bool check(std::vector<std::vector<char>> &board, int i, int j,
                   char val) {
-  int row = i - i % 3, col = j - j % 3;
-  for (int x = 0; x < 9; x++) // same column
-    if (board.at(x).at(j) == val)
+  for (int k = 0; k < 9; k++) {
+    if (board.at(i).at(k) == val)
       return false;
-  for (int y = 0; y < 9; y++) // same row
-    if (board.at(i).at(y) == val)
+    if (board.at(k).at(j) == val)
       return false;
-  for (int x = 0; x < 3; x++) // same block
-    for (int y = 0; y < 3; y++)
-      if (board.at(row + x).at(col + y) == val)
+  }
+  for (int x = (i / 3) * 3; x < (i / 3) * 3 + 3; x++)
+    for (int y = (j / 3) * 3; y < (j / 3) * 3 + 3; y++)
+      if (board.at(x).at(y) == val)
         return false;
   return true;
 }
 
 inline bool solve(std::vector<std::vector<char>> &board, int i, int j) {
-  if (i == 9) // buttom upper bound
+  if (i == 9)
     return true;
-  if (j == 9) // right upper bound
+  if (j == 9)
     return solve(board, i + 1, 0);
   if (board.at(i).at(j) != '.')
     return solve(board, i, j + 1);
-  for (char c = '1'; c <= '9'; c++) { // if current cell unknown
-    if (check(board, i, j, c)) {      // DFS
+  for (char c = '1'; c <= '9'; c++) {
+    if (check(board, i, j, c)) {
       board.at(i).at(j) = c;
       if (solve(board, i, j + 1))
         return true;
-      board.at(i).at(j) = '.'; // back and forward
+      board.at(i).at(j) = '.';
     }
   }
   return false;
